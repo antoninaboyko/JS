236731da7ed78d607b4114079ab1326e4626551c
antoninaboyko <a.lazyuta@gmail.com>
2023-04-19 23:41:30 +0300

    push

---
+++ Lesson_31/index31.js
@@ -0,0 +1,34 @@
+//1
+// let title = document.querySelector('.dom h2:nth-of-type(2)');
+// title.textContent = 'DOM method description';
+
+//2
+// let siteAll = document.querySelectorAll("a");
+// let siteUa = [];
+// for (let i = 0; i < siteAll.length; i++){
+//     if (siteAll[i].href.includes('/ua/')) {
+//         siteUa.push([String(siteAll[i].href)]);
+//     }
+// }
+// console.log(siteUa);
+
+//3
+// let list = document.querySelector('ul');
+// let nullLi = document.createElement('li');
+// nullLi.innerHTML = 0;
+// list.prepend(nullLi);
+// let firstLi = document.getElementById('first');
+// let secondLi = document.createElement('li');
+// secondLi.innerHTML = 2;
+// firstLi.append(secondLi);
+// let fourthLi = document.createElement('li');
+// fourthLi.innerHTML = 4;
+// list.append(fourthLi);
+
+//4
+// let title = document.querySelector('h1');
+// let link = document.createElement('a');
+// link.id = 'link';
+// link.href = "https://dom.spec.whatwg.org/";
+// link.appendChild(title);
+// document.body.appendChild(link);
