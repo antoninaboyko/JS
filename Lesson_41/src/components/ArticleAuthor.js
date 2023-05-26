74da18c3bbe5fdd817bcdec6496fd65375e987eb
antoninaboyko <a.lazyuta@gmail.com>
2023-05-26 18:39:49 +0300

    push

---
+++ Lesson_41/src/components/ArticleAuthor.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ArticleAuthor() {
+    return (
+        <div className="article__author">
+            <p className="article__author_text">Author: Mike</p>
+            <p className="article__author_text">Published: 06.12.2022</p>
+            <p className="article__author_text">Theme: Video cards</p>
+        </div>
+    );
+  }
+  export default ArticleAuthor;
