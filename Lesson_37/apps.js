37796cd5a2944779b3f827fd2c5d9826838cf05a
antoninaboyko <a.lazyuta@gmail.com>
2023-05-26 15:59:24 +0300

    push

--- Lesson_37/apps.js
+++ Lesson_37/apps.js
@@ -5,10 +5,6 @@ import { totalSalary } from "./methods.mjs";
 import { departmentsQuantity } from "./methods.mjs";
 import { departmentsSalary } from "./methods.mjs";
 
-$(document).ready(function() {
-    $('.form-control').addClass('is-invalid');
-  });
-
 console.log(totalSalary(employees));
 console.log(departmentsQuantity(employees));
 console.log(departmentsSalary(employees));
