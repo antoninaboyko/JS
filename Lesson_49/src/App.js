9c1030fa24ff79f34ff3a62ba1feffc7cdb36703
antoninaboyko <a.lazyuta@gmail.com>
2023-06-24 01:31:43 +0300

    push

--- Lesson_49/src/App.js
+++ Lesson_49/src/App.js
@@ -2,21 +2,35 @@ import './App.css';
 import Main from './components/Main';
 import Posts from './components/Posts';
 import Login from './components/Login';
+import SinglePost from './components/SinglePost';
+import {BrowserRouter, Routes, Route, NavLink} from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="wrapper">
       <nav className="menu">
-        <a href="#">Main</a>
-        <a href="#">Posts</a>
-        <a href="#">Login</a>
-      </nav>
+      <NavLink className={({isActive}) => isActive ? "menuLinkActive" : ""} to="/">Main</NavLink>
+          <NavLink className={({isActive}) => isActive ? "menuLinkActive" : ""} to="/posts">Posts</NavLink>
+          <NavLink className={({isActive}) => isActive ? "menuLinkActive" : ""} to="/login">Login</NavLink>
 
-      <Main />
+      </nav>
+      <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/posts' element={<Posts />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/post/:id' element={<SinglePost />}/>
+
+          <Route path='*' element={<h1>Not Found</h1>} />
+
+        </Routes>
+
+      {/* <Main />
       <Posts />
-      <Login />
+      <Login /> */}
 
     </div>
+    </BrowserRouter>
   );
 }
 
